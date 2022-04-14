44dc7e77c834fc75e6bb63668fd55628473fafa1
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-04-14 16:57:09 +0700

    feat: add QuarantineMedia to __init__.py

--- matrix_admin_sdk/endpoints/__init__.py
+++ matrix_admin_sdk/endpoints/__init__.py
@@ -2,4 +2,5 @@ from .account_validity import AccountValidity
 from .background_updates import BackgroundUpdates
 from .delete_group import DeleteGroup
 from .event_reports import EventReports
+from .quarantine_media import QuarantineMedia
 from .querying_media import QueryingMedia
